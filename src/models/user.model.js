48cef34bb6e54fce1e85f05b4a38e54baddf05b5
HanJeongSeol <hanjeong94@naver.com>
2023-03-06 16:54:13 +0900

    #4 Feat : DB 모델 생성

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,13 +1,19 @@
 const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
-  firstName: String,
-  lastName: String,
-  email: String,
-  password: String,
-  permissionLevel: Number,
+  userId: { type: String, required: true, unique: true },
+  userName: { type: String, required: true },
+  userEmail: { type: String, required: true, lowercase: true, unique: true },
+  userNickname: { type: String, minLength: 3, required: true, unique: true },
+  userBirth: { type: String },
+  userGender: { type: String },
+  userActive: { type: Boolean, default: 1 },
+  userImg: { type: String },
+  userJoin: { type: Date, default: Date.now },
+  userRole: { type: String, default: 'Normal' },
+  joinGroup: [{ type: mongoose.Schema.Types.ObjectId, required: true, ref: 'Group' }],
 });
 
-const User = mongoose.model('Users', userSchema);
+const User = mongoose.model('User', userSchema);
 
 module.exports = User;
