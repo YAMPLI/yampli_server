fc93cf3b8793f83c2d553c97fb9259b6f1454a5a
HanJeongSeol <hanjeong94@naver.com>
2023-03-10 14:05:55 +0900

    #5 Feat : req 객체 메소드 검증 파일 일원화
    API를 통제하기 위한 request 객체 메소드 param, query, body 메소드의 유효성 검증을 하나의 파일로 처리

--- src/api/middlewares/validate.js
+++ src/api/middlewares/validate.js
@@ -1,11 +1,21 @@
-const validate = (schema) => async (req, res, next) => {
-  const body = req.body;
-  try {
-    await schema.validateAsync(body);
-  } catch (err) {
-    return res.status(400).json({ code: 400, status: 'input_stringType' });
+const logger = require('../../config/logger');
+const Joi = require('joi');
+const pick = require('../../utils/pick');
+
+const validate = (schema) => (req, res, next) => {
+  const validSchema = pick(schema, ['params', 'query', 'body']);
+  const object = pick(req, Object.keys(validSchema));
+
+  const { value, error } = Joi.compile(validSchema)
+    .prefs({ errors: { label: 'key' }, abortEarly: false })
+    .validate(object);
+
+  if (error) {
+    logger.error(new Error(error));
+    return next(new Error(error));
   }
-  next();
+  Object.assign(req, value);
+  return next();
 };
 
 module.exports = validate;
