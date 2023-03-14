6335655c9957ad6af17232aeec196b8f632992f5
HanJeongSeol <hanjeong94@naver.com>
2023-03-14 15:56:11 +0900

    #8 Feat : passport-kakao설정
    passport 모듈을 사용하여 인증 전략 생성 및 임시 모델로 테스트

--- src/app.js
+++ src/app.js
@@ -2,8 +2,11 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const route = require('./api/routes');
-
 const conn = require('./config/conn.js');
+const session = require('express-session');
+const MemoryStore = require('memorystore')(session);
+const passport = require('passport');
+const passportConfig = require('./config/passport');
 
 const app = express();
 
@@ -13,6 +16,20 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 conn();
+passportConfig();
+app.use(
+  session({
+    cookie: { maxAge: 86400000 },
+    store: new MemoryStore({
+      checkPeriod: 86400000, // prune expired entries every 24h
+    }),
+    resave: false,
+    secret: 'keyboard cat',
+  }),
+);
+// passport-> 세션 설정 후 미들웨어 등록
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/api', route);
 
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,17 +1,23 @@
 const mongoose = require('mongoose');
 
+// const userSchema = mongoose.Schema({
+//   userId: { type: String, required: true, unique: true },
+//   userName: { type: String, required: true },
+//   userEmail: { type: String, required: true, lowercase: true, unique: true },
+//   userNickname: { type: String, minLength: 3, required: true, unique: true },
+//   userBirth: { type: String },
+//   userGender: { type: String },
+//   userActive: { type: Boolean, default: 1 },
+//   userImg: { type: String },
+//   userJoin: { type: Date, default: Date.now },
+//   userRole: { type: String, default: 'Normal' },
+//   joinGroup: [{ type: mongoose.Schema.Types.ObjectId, required: true, ref: 'Group' }],
+// });
+
+// Passport 테스트용
 const userSchema = mongoose.Schema({
-  userId: { type: String, required: true, unique: true },
-  userName: { type: String, required: true },
-  userEmail: { type: String, required: true, lowercase: true, unique: true },
-  userNickname: { type: String, minLength: 3, required: true, unique: true },
-  userBirth: { type: String },
-  userGender: { type: String },
-  userActive: { type: Boolean, default: 1 },
-  userImg: { type: String },
-  userJoin: { type: Date, default: Date.now },
-  userRole: { type: String, default: 'Normal' },
-  joinGroup: [{ type: mongoose.Schema.Types.ObjectId, required: true, ref: 'Group' }],
+  userId: { type: String, unique: true },
+  userName: { type: String },
 });
 
 const User = mongoose.model('User', userSchema);
