02bbe0ced12a97392b1e58e286e282749165ee25
HanJeongSeol <hanjeong94@naver.com>
2023-03-16 15:47:41 +0900

    #8 Feat : user.model, auth.validations 수정 후 테스트
    설계한 user컬렉션 기반으로 스키마와 검증 파일 수정 후 postman을 사용하여 테스트 진행

--- src/api/routes/auth.route.js
+++ src/api/routes/auth.route.js
@@ -1,55 +1,12 @@
-// const express = require('express');
-// const { authController } = require('../../controllers');
-// const router = express.Router();
-
-// router.route('/kakao/start').get(authController.kakaoLogin);
-
-// module.exports = router;
-
 const express = require('express');
 const { authController } = require('../../controllers');
 const passport = require('passport');
 const router = express.Router();
 
-// router.route('/kakao/start').get(authController.kakaoLogin);
 router.route('/kakao').get(passport.authenticate('kakao'));
 router.route('/kakao/callback').get(passport.authenticate('kakao', { failureRedirect: '/' }), (req, res, next) => {
-  res.route('/login');
+  console.log(`req : ${req}`);
+  res.redirect('/api/auth');
 });
-// router.route('/test').get((req, res) => {
-//   res.redirect('../../../login.html');
-// });
-// router.route('/kakao/callback').get((req, res, next) => {
-//   passport.authenticate('kakao', { failureRedirect: '/' }, (err, user, info) => {
-//     console.log('user ' + user);
-//     // passport-kakao 전략 done 함수의 파라미터가 여기 콜백 함수의 인자로 전달된다.
-//     if (err) {
-//       return next(err);
-//     }
-//     if (!user) {
-//       const { provider, id } = info;
-//       // 등록된 회원 정보가 없을 경우 info 파라미터에 전달된 사용자 정보를 이용해 세션을 생성한다.
-//       req.session.joinUser = {
-//         provider,
-//         snsId: id,
-//         email: info._json.kakao_account.email,
-//         nickname: info._json.properties.nickname,
-//         profile: info._json.properties.profile_image,
-//       };
-//       return req.session.save(() => {
-//         // 세션이 생성되면 사용자를 회원가입 페이지로 리다이렉트 시킨다.
-//         res.redirect(`${frontServer}/signup`);
-//       });
-//     }
-
-//     // 회원가입된 상태일 경우, 로그인 세션을 생성한다.
-//     return req.login(user, (error) => {
-//       if (error) {
-//         next(error);
-//       }
-//       res.redirect('/');
-//     });
-//   })(req, res, next); // 미들웨어 내의 미들웨어에는 호출 별도로 진행
-// });
 
 module.exports = router;
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,23 +1,17 @@
 const mongoose = require('mongoose');
 
-// const userSchema = mongoose.Schema({
-//   userId: { type: String, required: true, unique: true },
-//   userName: { type: String, required: true },
-//   userEmail: { type: String, required: true, lowercase: true, unique: true },
-//   userNickname: { type: String, minLength: 3, required: true, unique: true },
-//   userBirth: { type: String },
-//   userGender: { type: String },
-//   userActive: { type: Boolean, default: 1 },
-//   userImg: { type: String },
-//   userJoin: { type: Date, default: Date.now },
-//   userRole: { type: String, default: 'Normal' },
-//   joinGroup: [{ type: mongoose.Schema.Types.ObjectId, required: true, ref: 'Group' }],
-// });
-
-// Passport 테스트용
 const userSchema = mongoose.Schema({
-  userId: { type: String, unique: true },
-  userName: { type: String },
+  userId: { type: String, required: true, unique: true },
+  userName: { type: String, required: true },
+  userEmail: { type: String, required: true, lowercase: true, unique: true },
+  userNickname: { type: String, minLength: 3, maxLength: 30, required: true },
+  userBirth: { type: String },
+  userGender: { type: String },
+  userActive: { type: Boolean, default: 1 },
+  userImg: { type: String },
+  userJoin: { type: String },
+  userRole: { type: String, default: 'Normal' },
+  joinGroup: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Group' }],
 });
 
 const User = mongoose.model('User', userSchema);
