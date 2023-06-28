ecd7673275f1b98d42c772c84f5211b7e5503567
HanJeongSeol <hanjeong94@naver.com>
2023-06-28 14:52:37 +0900

    #9  Feat : 로그인 , 회원가입 수정
    프론트 서버에서 카카오 로그인 요청 받은 후 kakao-passport에 정의한 로그인 전략 수행

--- src/api/routes/auth.route.js
+++ src/api/routes/auth.route.js
@@ -3,10 +3,30 @@ const { authController } = require('../../controllers');
 const passport = require('passport');
 const router = express.Router();
 
-router.route('/kakao').get(passport.authenticate('kakao'));
-router.route('/kakao/callback').get(passport.authenticate('kakao', { failureRedirect: '/' }), (req, res, next) => {
-  console.log(`req : ${req}`);
-  res.redirect('/api/auth');
+// 로그인 여부 체크
+router.route('/authcheck').get((req, res) => {
+  const sendData = { isLogin: '' };
+  if (req.session.is_loginsed) {
+    sendData.isLogin = 'True';
+  } else {
+    sendData.isLogin = 'False';
+  }
+  res.send(sendData);
 });
 
+router.route('/kakao').get(passport.authenticate('kakao'), () => {
+  console.log('/kakao');
+});
+router.route('/kakao/callback').get(passport.authenticate('kakao', { failureRedirect: '/api' }), (req, res) => {
+  console.log(`req : ${res}`);
+  res.redirect('http://localhost:3000/');
+});
+
+// router.route('/kakao/callback').get(
+//   passport.authenticate('kakao', (authError, user, info) => {
+//     console.log(authError);
+//     console.log(user);
+//     console.log(info);
+//   }),
+// );
 module.exports = router;
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -4,12 +4,9 @@ const userSchema = mongoose.Schema({
   userId: { type: String, required: true, unique: true },
   userName: { type: String, required: true },
   userEmail: { type: String, required: true, lowercase: true, unique: true },
-  userNickname: { type: String, minLength: 3, maxLength: 30, required: true },
-  userBirth: { type: String },
-  userGender: { type: String },
+  userNickname: { type: String, minLength: 1, maxLength: 100, required: true },
   userActive: { type: Boolean, default: 1 },
   userImg: { type: String },
-  userJoin: { type: String },
   userRole: { type: String, default: 'Normal' },
   joinGroup: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Group' }],
 });
