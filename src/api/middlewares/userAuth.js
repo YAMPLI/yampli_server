b3b6ae9b585837dddd4c35f4bf2aaffb5aa2f028
HanJeongSeol <hanjeong94@naver.com>
2023-07-07 15:14:27 +0900

    #15 Feat : userAuth 미들웨어로 접속 유저 체크
    userAuth 미들웨어를 사용하여 로그인 해야 접근 가능한 route에서 유저 검증을 진행하도록 한다

---
+++ src/api/middlewares/userAuth.js
@@ -0,0 +1,27 @@
+/**
+ * 유저 로그인 후 클라이언트와 통신할 때 마다 헤더에 토큰을 담아와서 db에 저장된 유저 데이터와 비교한다.
+ */
+
+const jwt = require('jsonwebtoken');
+const { UnauthenticatedError } = require('../../utils/errors');
+const { userService } = require('../../services');
+
+const userAuth = async (req, res, next) => {
+  // 클라이언트에서 헤더에 유저 토큰을 담아온다
+  try {
+    const authHeader = req.headers.authorization;
+    // 토큰 데이터가 오지 않거나 Bearer 문자열로 시작되는지 데이터가 없을 시 조건이 True가 되면서 에러를 던진다.
+    if (!authHeader || !authHeader.startsWith('Bearer')) {
+      throw new UnauthenticatedError('로그인이 필요합니다.');
+    }
+    const token = authHeader.split(' ')[1];
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+
+    const user = await userService.getUserById(payload.userId);
+    req.locals = user;
+    next();
+  } catch (error) {
+    throw new UnauthenticatedError('로그인이 필요합니다.');
+  }
+};
+module.exports = userAuth;
