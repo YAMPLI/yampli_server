8c75a52f36a82f9aac695d9daa203ab92950336f
HanJeongSeol <hanjeong94@naver.com>
2023-07-17 17:20:38 +0900

    #15 Feat : 클라이언트에 플리 정보 전송
    전달받은 플리 id로 플리, 참조되어있는 노래 데이터 클라이언트로 전송

--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const authRoute = require('./auth.route');
 const userRoute = require('./user.route');
 const groupRoute = require('./group.route');
+const playlistRoute = require('./playlist.route');
 const userAuth = require('../middlewares/userAuth');
 // const docsRoute = require("./docs.route");
 
@@ -9,6 +10,8 @@ const router = express.Router();
 
 router.use('/auth', authRoute);
 router.use('/users', userRoute);
+// userAuth,
 router.use('/group', userAuth, groupRoute);
+router.use('/playlist', playlistRoute);
 
 module.exports = router;
