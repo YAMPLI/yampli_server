a84fa26b1e253433861f29364a6f4a770c0dc130
HanJeongSeol <hanjeong94@naver.com>
2023-08-31 17:58:08 +0900

    테스트 파일 제외

--- apiTest/youtube.service.js
+++ apiTest/youtube.service.js
@@ -14,15 +14,17 @@ const test1 = [
   '$ 아이유,라일락, 이거 좋더라',
   '생성 오후 6:01',
   '$인피니트, 내꺼하자, 얘네 컴백했더라, 댄스',
-  '중간에 추천하면? $zb1, 인불름, 서바이벌 1위한 애들이래,최신곡 오후 6:02',
-  '$$ 백예린,스퀘어,이거 가을날씨에 듣기 딱임, 감성',
+  '중간에 추천하면? $zerobaseone, 인불름, 서바이벌 1위한 애들이래,최신곡 오후 6:02',
+  '$$ baek yerin ,스퀘어,이거 가을날씨에 듣기 딱임, 감성',
   '얌플리데이터\\',
   '생성중',
-  '$$ 아이유,라일락, 이거 좋더라',
+  '$$ IU,라일락, 이거 좋더라',
   '생성',
-  '$$인피니트, 내꺼하자, 얘네 컴백했더라, 댄스 오후 6:14',
-  '중간에 추천하면? $$zb1, 인블름, 서바이벌 1위한 애들이래,최신곡 오후 6:15',
+  '$$infinite, 내꺼하자, 얘네 컴백했더라, 댄스 오후 6:14',
+  '중간에 추천하면? $$zb1, inbloome, 서바이벌 1위한 애들이래,최신곡 오후 6:15',
   '2023년 8월 3일 목요일',
+  '$$ghost9 , Fever, 피크타임이래',
+  '$$배너 , performer, 피크타임이래',
   '기본중명서 , 가족관계중명서, 오후 1:23',
   '2023년 8월 8일 화요일',
   '아빠 입금 오후 11:46',
@@ -35,6 +37,9 @@ const test = [
   '중간에 추천하면? $zb1, 인불름, 서바이벌 1위한 애들이래,최신곡 오후 6:02',
   '중간에 추천하면? $$zb1, 인블름, 서바이벌 1위한 애들이래,최신곡 오후 6:15',
 ];
+
+const test2 = ['$$ghost9 , Fever, 피크타임이래'];
+
 const musicList = textData(test1);
 
 const saveVideo = () => {
@@ -42,9 +47,10 @@ const saveVideo = () => {
     const [artist, title, comment, tag] = music;
     // 유튜브
     // const videoData = await findVideo(artist, title);
-    // 스포티파이
+    // 스포티파이;
     const videoData = await spotifyApi(artist, title);
     if (videoData) {
+      console.log(videoData.images);
       console.log(`${artist} : ${title} : ${videoData.name} : ${videoData} `);
     }
   });
@@ -52,26 +58,35 @@ const saveVideo = () => {
 
 // api를 사용하지 않고 데이터에서 직접 추출
 const findVideo = async (artist, title) => {
-  const word = `${artist} ${title} audio`;
+  // const word = `${artist} ${title} audio`;
+  // 'by' 연관성 높임
+  const word = `${title} by ${artist} audio`;
   const response = await axios.get(`https://www.youtube.com/results?search_query=${word}`);
   let regex = /\"/g;
   // 영상 제목(가수 - 노래제목), 스포티파이 검색 결과 정확도 높이기 위해서 사용
   const check = response.data
     .split('{"videoRenderer":{"videoId":"')[1]
-    .split('{"text":')[1]
-    .split('}]')[0]
-    .replace(regex, '');
+    // .split(',thumbnail')[1]
+    // .split('{"text":')[1]
+    // .split('}]')[0]
+    .replace(regex, '')
+    .split(',thumbnail:')[0];
   fs.appendFileSync('check-json1.html', check + '\n');
-  return response.data.split('{"videoRenderer":{"videoId":"')[1].split('"')[0];
+  // return response.data.split('{"videoRenderer":{"videoId":"')[1].split('"')[0];
+  return check;
 };
 
 const spotifyApi = async (artist, title) => {
   const token = await getValidToken();
+  // 검색 결과 undefined인 경우 에러 처리
   const query = `"${artist}" "${title}"`;
+  // console.log(await findVideo(artist, title));
+
+  // const query = await findVideo(artist, title);
   const params = {
     q: query,
     type: 'track',
-    limit: 3,
+    limit: 1,
   };
   const url = `https://api.spotify.com/v1/search?${qs.stringify(params)}`;
   const headers = {
@@ -80,10 +95,10 @@ const spotifyApi = async (artist, title) => {
 
   try {
     const response = await axios.get(url, { headers: headers });
+    // console.log(response.data.artists.items);
     if (response.data.tracks.items.length > 0) {
-      console.log(`===============${artist} : ${title}===================`);
-      console.log(response.data.tracks.items[0].album);
-      return response.data.tracks.items[0];
+      return response.data.tracks.items[0].album;
+      // return response.data.tracks.items;
     } else {
       return null;
     }
